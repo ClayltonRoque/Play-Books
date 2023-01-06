6decac4ae7304d90057609729973b60a6b18c9c3
Claylton Roque (FB Online) <claylton.roque@fariasbrito.com.br>
2023-01-06 17:56:13 -0300

    estrutura incial Navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,18 @@
 <template>
-  <Nuxt />
+  <div>
+    <Navbar />
+    <Nuxt />
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+import Navbar from "../components/Navbar.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Navbar,
+  },
+});
+</script>
