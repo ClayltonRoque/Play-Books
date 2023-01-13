b7d90ab55b337070de7a05978e816057e1d48465
Alex Candido <80834340+alex-candido@users.noreply.github.com>
2023-01-13 16:06:10 -0300

    adicionado propriedades globais em theme

--- components/BookCard.vue
+++ components/BookCard.vue
@@ -1,67 +1,67 @@
 <template>
-  <div class="column is-4-desktop is-12-tablet is-justify-content-center is-3" style="display: flex; cursor: pointer;"  @click="$router.push({path: `/details/${book.id}`})">
-    <div class="card is-desktop" style="max-width: 392px">
-      <div class="card-content">
-        <div class="columns">
-          <div class="column ">
-            <img :src="book.volumeInfo.imageLinks.thumbnail">
-          </div>
-          <div class="column">
-            <div class="content">
-              <p class="title is-size-4 has-text-base-title">
-                {{ book.volumeInfo.title }}
-              </p>
-              <p class="subtitle has-text-base-subtitle">
-                {{ book.volumeInfo.authors }}
-              </p>
-              <p class="description has-text-base-text is-size-6">{{ book.volumeInfo.description }}</p>
+  <div class="play-book-bookcard" @click="$router.push({path: `/details/${book.id}`})">
+    <div class="card is-desktop">
+        <div class="card-content">
+          <div class="columns">
+            <div class="column ">
+              <img :src="book.volumeInfo.imageLinks.thumbnail">
+            </div>
+            <div class="column">
+              <div class="content">
+                <p class="title is-size-4 has-text-base-title">
+                  {{ book.volumeInfo.title }}
+                </p>
+                <p class="subtitle has-text-base-subtitle">
+                  {{ book.volumeInfo.authors }}
+                </p>
+                <p class="description has-text-base-text is-size-6">{{ book.volumeInfo.description }}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "@nuxtjs/composition-api";
 
 export default defineComponent({
   name: "playBookCard",
   props: ["book"],
   setup () {
-    const id = "MhNhBgAAQBAJ"
-
-    return {
-      id,
-    }
+    return {}
   }
 })
 </script>
 
-<style lang="scss" scoped>
-  img {
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    border-radius: 10px;
-    height: 100%;
+<style lang="scss">
+  .play-book-bookcard {
+    img {
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: cover;
+      border-radius: 10px;
+      height: 100%;
 
-  border: 1px solid #112131;
-    &:hover {
-    border-color: #00acee;
-  }
+      border: 1px solid #112131;
+        &:hover {
+        border-color: #00acee;
+      }
 
   &:disabled {
     opacity: 0;
     transform: scale(0);
   }
+
   &:hover {
     opacity: 1;
     transform: scale(1.1);
   }
+
   transition: all 0.2s;
   }
+
   .title {
     text-overflow: ellipsis;
     display: -webkit-box;
@@ -74,6 +74,7 @@ export default defineComponent({
     display: -webkit-box;
     -webkit-box-orient: vertical;
   }
+
   .description {
     /* color: #afc2d4; */
     overflow: hidden;
@@ -82,11 +83,12 @@ export default defineComponent({
     -webkit-box-orient: vertical;
     -webkit-line-clamp: 3;
   }
-
+  
   .card {
     border: 1px solid #112131;
     &:hover {
     border-color: #00acee;
-  }
+      }
+    }
   }
 </style>
--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -1,17 +1,18 @@
 <template>
   <section class="container" style="top: 86px">
     <div class="py-5">
-      <p class="has-text-base-title">Meu Id: {{ this.$route.params.id }}</p>
+      <p class="has-text-base-title">Meu Id: {{ $route.params.id }}</p>
     </div>
   </section>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {};
-  },
-};
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+export default defineComponent({
+  setup () {
+    return {}
+  }
+});
 </script>
 
 <style lang="scss" scoped></style>
