0579e8a2e6177780c2d318ac8f1624758f917af2
ClayltonRoque <claylton202@gmail.com>
2023-02-23 17:57:26 -0300

    animação

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <section class="play-books-home container" style="top: 86px">
-    <div v-if="books.length" class="columns is-multiline align-items-full py-5">
-      <PlayBookCard
-        v-for="(book, index) in books"
-        :key="index"
-        :book="book"
-        class="play-book-card column is-4-desktop is-12-tablet is-justify-content-center is-3"
-      />
+    <div>
+      <TransitionGroup
+        class="columns is-multiline align-items-full py-5"
+        tag="div"
+        :css="false"
+        @before-enter="onBeforeEnter"
+        @leave="onLeaveCard"
+        @enter="onEnterCard"
+      >
+        <div
+          v-for="(book, index) in books"
+          :key="book.id"
+          class="column book-column is-4-desktop is-12-tablet is-justify-content-center is-3"
+          :data-index="index"
+        >
+          <PlayBookCard :book="book" class="play-book-card" />
+        </div>
+      </TransitionGroup>
     </div>
     <BooksPagination v-if="totalBooks" />
     <div v-else>
@@ -17,13 +28,12 @@
 
 <script lang="ts">
 import { defineComponent, reactive, watch } from '@nuxtjs/composition-api'
+import gsap from 'gsap'
 
 import { useBookData } from '~/service/bookData'
 
 import PlayBookCard from '~/components/BookCard.vue'
-
 import NoPageContent from '~/components/NoPageContent.vue'
-
 import BooksPagination from '~/components/BooksPagination.vue'
 
 export default defineComponent({
@@ -34,15 +44,47 @@ export default defineComponent({
     const state = reactive({
       activePagination: false,
     })
+
     watch(books, () => {
       if (books.value.length) {
         state.activePagination = true
       }
     })
+
+    function onBeforeEnter(el: any) {
+      el.style.opacity = 0
+    }
+
+    function onEnterCard(el: any, done: any) {
+      gsap.fromTo(
+        el,
+        {
+          y: '50%',
+        },
+        {
+          opacity: 1,
+          y: 0,
+          delay: el.dataset.index * 0.15,
+          onComplete: done,
+        }
+      )
+    }
+
+    function onLeaveCard(el: any) {
+      el.style.position = 'absolute'
+      el.style.opacity = 0
+      // gsap.to(el, {
+      //   opacity: 0,
+      // })
+    }
+
     return {
       books,
       totalBooks,
       state,
+      onEnterCard,
+      onBeforeEnter,
+      onLeaveCard,
     }
   },
 })
